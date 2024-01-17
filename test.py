3db41052d0a95b3fd131d46d577a112c6c6dfb7d
KSPROKS <156220204+KSPROKS@users.noreply.github.com>
2024-01-17 19:00:17 +0300

    tip dönüştürmeleri
    tip dönüştürmeleri

---
+++ test.py
@@ -0,0 +1,26 @@
+#a=-5
+#print(abs(a))
+
+#5==5
+
+
+#us=pow(2,3)
+#print(us)
+
+#from math import sqrt
+#değişken=256
+#karekok=sqrt(değişken)
+#print("sayinin karekodu",karekok)
+
+
+
+#from math import ceil
+#from math import floor
+#ondalik=423.400
+#değişken=ceil(ondalik)
+#değişken2=floor(ondalik)
+#print("yukari",değişken2)
+#print("aşaği",değişken)
+
+
+
---
+++ test2.py
@@ -0,0 +1,43 @@
+sayi1 = 34
+sayi2 = 82
+#print (type(sayi1))
+#print (type(sayi2))
+
+
+sayi3 = 1/2
+sayi4 = -5.5
+#print (type(sayi3))
+#print (type(sayi4))
+
+
+dogru_mu = True
+yanlis_mi = False
+#print (type(dogru_mu))
+#print (type(yanlis_mi))
+
+
+sayi_float = float(sayi1)
+#print ("int to float",sayi_float)
+
+
+sayi_int = int(sayi3) 
+#print ("float to int",sayi_int)
+
+
+sayi_bool = int(dogru_mu)
+#print ("bool to int",sayi_bool)
+
+#print (type(sayi_bool))
+
+
+#kullanici_ad =input("lütfen kullanici adinizi girniz:")
+#print ("Hoşgeldin",kullanici_ad)
+
+
+#sayi5 =int(input("lütfen 1.sayiyi giriniz:"))
+#sayi6 =int(input("lütfen 2.sayiyigiriniz:"))
+#print (sayi5+sayi6)
+
+#sayi5 =int(input("lütfen 1.sayiyi giriniz:"))
+#sayi6 =int(input("lütfen 2.sayiyigiriniz:"))
+#print (int(sayi5)+int(sayi6))
